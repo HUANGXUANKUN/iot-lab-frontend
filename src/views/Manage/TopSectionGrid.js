85114de84774cfd50298437224b0ff7840da56ec
NingLiu006 <liuningntu@gmail.com>
2020-06-28 19:08:12 +0800

    add button control section in devices manage page

---
+++ src/views/Manage/TopSectionGrid.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import NewButton from './NewButton';
+
+const ContainerStyle = styled.div`
+    display: flex;
+    margin: 5px;
+    margin-right: 10px;
+    margin-left: auto;
+    max-width: fit-content;
+`
+
+const ButtonStyle = styled.div`
+    margin: 0px 5px;    
+`
+
+export default function (props) {
+    return (
+        <ContainerStyle>
+
+            <ButtonStyle>
+                <NewButton link="/device/new">New Hub</NewButton>
+            </ButtonStyle>            
+            <ButtonStyle>
+                <NewButton link="/device/new">New Node</NewButton>
+            </ButtonStyle>
+        </ContainerStyle>
+    )
+}
